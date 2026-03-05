98da94e7251847f03d8eaccb4c93b45142672fde
Michael _377 <yelubin377@gmail.com>
2026-03-05 20:53:09 +0800

    Add files via upload

--- JinCheng.c
+++ JinCheng.c
@@ -8,18 +8,42 @@
 
 // }
 
-#include<stdio.h>
-#include<stdlib.h>
-int a = 0;
-int b;
-int main()
-{
-    int c = 0;
-    int *d = (int*)malloc(sizeof(int));
-    printf("a:%p\n",&a);
-    printf("b:%p\n",&b);
-    printf("c:%p\n",&c);
-    printf("*d:%p\n",&d);
-    free(d);
-    return 0;
-}
+
+// //复习一下进程的内存分布： 文本段，数据段，BSS，栈，堆。
+// #include<stdio.h>
+// #include<stdlib.h>
+// int a = 0;
+// int b;
+// int main()
+// {
+//     int c = 0;
+//     int *d = (int*)malloc(sizeof(int));
+//     printf("a:%p\n",&a);
+//     printf("b:%p\n",&b);
+//     printf("c:%p\n",&c);
+//     printf("*d:%p\n",&d);
+//     free(d);
+//     return 0;
+// }
+
+
+
+// //命令行参数
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main(int argc,char *argv[])
+// {
+//     printf("argc: %d\n",argc);
+//     for(int i = 0;i<argc;i++)
+//     {
+//         printf("argv[%d] = %s\n",i,argv[i]);
+//         char **p;
+//         for(p=argv;*p!=NULL;p++)
+//         {
+//             puts(*p);
+//         }
+//         exit(EXIT_SUCCESS);
+//     }
+// }
+
+
