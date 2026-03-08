4992efbde03e00adb97e0007db9b064726977104
Michael _377 <yelubin377@gmail.com>
2026-03-08 19:02:06 +0800

    Add files via upload

---
+++ Shell-practice.c
@@ -0,0 +1,15 @@
+
+//fgets
+// #include<stdio.h>
+// #define MaxLen 4096
+// int main()
+// {
+//     char line[MaxLen];
+//     printf("请输入命令：");
+//     fgets(line,MaxLen,stdin);
+//     printf("你输入的命令是：%s",line);
+//     return 0;
+// }
+
+
+
